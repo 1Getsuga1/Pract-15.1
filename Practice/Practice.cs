b45ebf37653de118848629ef46d9fcf6d504a454
Кирилл Соколов <sokolov17925@yandex.ru>
2021-12-26 21:13:35 +0300

    Добавьте файлы проекта.

---
+++ Practice/Practice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    public class Practice
+    {
+        static Random random = new Random();
+        public static (double allNumbers, string calculations) ProductEvenWithOut(int count)
+        {
+
+            int product = 1;
+            string numbers = string.Empty;
+
+            for (int i = 0; i < count; i++)
+            {
+                int number = random.Next(0, count);
+
+                numbers += number.ToString() + " ";
+
+                product *= number;
+            }
+
+            var finding = (product, numbers);
+            return finding;
+        }
+    }
+}
